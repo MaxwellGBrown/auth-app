01446ba3c2d1c691019866d2bb0f5c5c57019fcb
Maxwell Brown <maxwellgbrown@gmail.com>
2017-02-09 10:07:19 -0500

    login view now handles authentication

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,4 +1,8 @@
 from pyramid.view import view_config, view_defaults
+import pyramid.httpexceptions as http
+from pyramid.security import remember
+
+from auth_app.models import User
 
 
 @view_config(route_name='index', renderer='index.mako')
@@ -18,4 +22,13 @@ class LoginViews(object):
 
     @view_config(request_method="POST")
     def post_login(self):
-        return {}
+        print(self.request.POST)
+        user = User.one(email=self.request.POST.get('email'))
+        print(user.email, user._password)
+        if user.validate(self.request.POST.get('password')) is True:
+            raise http.HTTPFound(
+                self.request.route_url('index'),
+                headers=remember(self.request, user.user_id)
+            )
+        else:
+            return {}
