7ce4e76acf3ccc09d2c11d1ad4f8a4002c51ce80
Maxwell Brown <maxwellgbrown@gmail.com>
2017-02-09 09:55:11 -0500

    added AppBase declarative base mixin for class method queries

--- auth_app/models.py
+++ auth_app/models.py
@@ -9,7 +9,25 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 Session = scoped_session(sessionmaker())
-Base = declarative_base()
+
+
+class AppBase(object):
+    """ A declarative base mixin that lets you run queries as cls methods """
+
+    @classmethod
+    def query(cls, **kwargs):
+        return Session.query(cls).filter_by(**kwargs)
+
+    @classmethod
+    def one(cls, **kwargs):
+        return cls.query(**kwargs).one()
+
+    @classmethod
+    def all(cls, **kwargs):
+        return cls.query(**kwargs).all()
+
+
+Base = declarative_base(cls=AppBase)
 
 
 def bind_engine(engine, create_all=False):
