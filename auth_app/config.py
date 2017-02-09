2f9ec33a1178d83f15e07d7d734cbbd0ff9a2da5
Maxwell Brown <maxwellgbrown@gmail.com>
2017-02-09 10:21:09 -0500

    implemented RootFactory w/ permission "authenticated"

--- auth_app/config.py
+++ auth_app/config.py
@@ -1,10 +1,20 @@
 from pyramid.config import Configurator
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
+import pyramid.security as security
 from sqlalchemy import engine_from_config
 
 from auth_app.models import request_user, auth_callback
 import auth_app.models as app_model
+
+
+class RootFactory(object):
+    __acl__ = [
+        (security.Allow, security.Authenticated, 'authenticated')
+    ]
+
+    def __init__(self, request):
+        self.request = request
 
 
 def main(global_config, **settings):
@@ -26,7 +36,7 @@ def main(global_config, **settings):
     # authorization
     authz_policy = ACLAuthorizationPolicy()
     config.set_authorization_policy(authz_policy)
-    # config.set_root_factory(RootFactory)
+    config.set_root_factory(RootFactory)
 
     # request methods
     config.add_request_method(request_user, "user", reify=True)
