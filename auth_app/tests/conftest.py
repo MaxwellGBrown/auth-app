3509beca9927c0c2c56488128992829b2ce9b184
Maxwell Brown <maxwellgbrown@gmail.com>
2017-02-10 14:09:26 -0500

    added test_user fixture which returns transient user

--- auth_app/tests/conftest.py
+++ auth_app/tests/conftest.py
@@ -3,6 +3,11 @@ import configparser
 import alembic
 from alembic.config import Config
 import pytest
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import make_transient
+
+import auth_app.models as app_model
 
 
 def pytest_addoption(parser):
@@ -35,3 +40,34 @@ def alembic_head(request, ini_filepath):
         alembic.command.downgrade(alembic_cfg, "base")
 
     request.addfinalizer(alembic_base)
+
+
+@pytest.fixture(scope="session")
+def test_user(request, ini_config, alembic_head):
+    """ Returns a transient User object that represents an object in the db """
+    engine = engine_from_config(
+        configuration=ini_config['app:main'],
+        prefix="sqlalchemy."
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = app_model.User(
+        email="test@example.com",
+        password="password123"
+    )
+
+    session.add(user)
+    session.commit()
+
+    session.refresh(user)
+    make_transient(user)
+
+    session.close()
+
+    def remove_test_user():
+        session = Session()
+        session.delete(user)
+        session.commit()
+
+    return user
